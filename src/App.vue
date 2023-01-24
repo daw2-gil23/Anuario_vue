08f1863aa8823fba779b2ed7684cb29cf6973a6c
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2023-01-24 19:51:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <TheHeader/>
+  <router-link to="/"></router-link>
+  <router-view />
+</template>
+
+<script>
+  import TheHeader from './components/TheHeader.vue';
+  import TheMain from './components/TheMain.vue';
+
+
+  export default {
+    name: "App",
+    components:{
+      TheHeader,
+      TheMain
+    }
+  };
+</script>
+
